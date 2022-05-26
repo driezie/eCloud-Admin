6a0dec8e7e8a3bec414217890b2c8131a7ebe589
Drieziee <80174866+driezie@users.noreply.github.com>
2022-05-26 21:28:35 +0200

    d

--- admin/actions/pushing.php
+++ admin/actions/pushing.php
@@ -8,85 +8,96 @@ $data3 = array();
 $data4 = array();
 
 array_push($data, array("Dag", "Aantal"));
+
+array_push($data, array("2022-05-01", "1"));
+
 array_push($data2, array("Dag", "Aantal"));
 array_push($data3, array("ja", "nee"));
 array_push($data4, array("ja", "nee"));
 
-for ($i = 0; $i < 30; $i++) {
+echo '<pre>';
+print_r($data) ;
+echo '</pre>';
+
+// for ($i = 0; $i < 30; $i++) {
     
-    $days[$i] = date('Y-m-d', strtotime('-'.$i.' days'));
+//     $days[$i] = date('Y-m-d', strtotime('-'.$i.' days'));
 
     
-    // get count of users on the day
-    $sql = "SELECT COUNT(*) FROM users WHERE account_created = :account_created AND administrator_role = 'N'";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(':account_created', $days[$i]);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    $count_account_created = $result['COUNT(*)'];
-    array_push($data, array($days[$i], $count_account_created)); 
-}
+//     // get count of users on the day
+//     $sql = "SELECT COUNT(*) FROM users WHERE account_created = :account_created AND administrator_role = 'N'";
+//     $stmt = $dbh->prepare($sql);
+//     $stmt->bindParam(':account_created', $days[$i]);
+//     $stmt->execute();
+//     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//     $count_account_created = $result['COUNT(*)'];
+//     // array_push($data, array($days[$i], $count_account_created)); 
+// }
 
 // echo '<pre>';
 // print_r($data) ;
 // echo '</pre>';
 
 
-// echo '<br><br>';
+// // echo '<br><br>';
 
 
-for ($i = 0; $i < 30; $i++) {
+// for ($i = 0; $i < 30; $i++) {
 
 
-    $days[$i] = date('Y-m-d', strtotime('-'.$i.' days'));
-    $sql = "SELECT COUNT(*) FROM files WHERE file_upload_date = :file_upload_date";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(':file_upload_date', $days[$i]);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    $count_files = $result['COUNT(*)'];
+//     $days[$i] = date('Y-m-d', strtotime('-'.$i.' days'));
+//     $sql = "SELECT COUNT(*) FROM files WHERE file_upload_date = :file_upload_date";
+//     $stmt = $dbh->prepare($sql);
+//     $stmt->bindParam(':file_upload_date', $days[$i]);
+//     $stmt->execute();
+//     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//     $count_files = $result['COUNT(*)'];
     
-    array_push($data2, array($days[$i], $count_files));   
+//     array_push($data2, array($days[$i], $count_files));   
 
-}
+// }
 
-$sql = "SELECT DISTINCT file_type FROM files";
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($result as $row) {
-        $sql = "SELECT COUNT(*) FROM files WHERE file_type = :file_type";
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(':file_type', $row['file_type']);
-        $stmt->execute();
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $count_style_type = $result['COUNT(*)'];
-        array_push($data3, array($row['file_type'], $count_style_type)); 
-    }
+// // echo '<pre>';
+// // print_r($data2);
+// // echo '</pre>';
+
+// $sql = "SELECT DISTINCT file_type FROM files";
+// $stmt = $dbh->prepare($sql);
+// $stmt->execute();
+// $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// foreach ($result as $row) {
+//         $sql = "SELECT COUNT(*) FROM files WHERE file_type = :file_type";
+//         $stmt = $dbh->prepare($sql);
+//         $stmt->bindParam(':file_type', $row['file_type']);
+//         $stmt->execute();
+//         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//         $count_style_type = $result['COUNT(*)'];
+//         array_push($data3, array($row['file_type'], $count_style_type)); 
+//     }
       
-// echo '<pre>';
-// print_r($data3);
-// echo '</pre>';
+// // echo '<pre>';
+// // print_r($data3);
+// // echo '</pre>';
 
 
 
-$sql = "SELECT DISTINCT file_type FROM files";
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($result as $row) {
-        $sql = "SELECT SUM(file_size) FROM files WHERE file_type = :file_type";
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(':file_type', $row['file_type']);
-        $stmt->execute();
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $count_style_type = $result['SUM(file_size)'];
-        array_push($data4, array($row['file_type'], $count_style_type));
-    }
+// $sql = "SELECT DISTINCT file_type FROM files";
+// $stmt = $dbh->prepare($sql);
+// $stmt->execute();
+// $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// foreach ($result as $row) {
+//         $sql = "SELECT SUM(file_size) FROM files WHERE file_type = :file_type";
+//         $stmt = $dbh->prepare($sql);
+//         $stmt->bindParam(':file_type', $row['file_type']);
+//         $stmt->execute();
+//         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//         $count_style_type = $result['SUM(file_size)'];
+//         array_push($data4, array($row['file_type'], $count_style_type));
+//     }
       
-// echo '<pre>';
-// print_r($data4);
-// echo '</pre>';
+// // echo '<pre>';
+// // print_r($data4);
+// // echo '</pre>';
 
 
 
